f18a9a3696d5a935525a9acb9bb75980a87ff7a2
punit-zeta <punits@zeta.tech>
2021-02-24 00:08:27 +0530

    Intial commit

---
+++ src/Testing/JUnitTestSuite.java
@@ -0,0 +1,14 @@
+package Testing;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        // All test files
+        JUnitTest.class,
+        JUnitTestCopy.class
+})
+public class JUnitTestSuite {
+}
